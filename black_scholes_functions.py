7328c11c18f7bf3470d6580796853ca353f90adb
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-23 12:48:26 +0100

    made calculate_d1_d2 function

--- black_scholes_functions.py
+++ black_scholes_functions.py
@@ -25,3 +25,9 @@ from scipy.stats import norm
 # d1 and d2 are intermediate variables calculated as:
 # d1 = (ln(S0/K)+((r+o^2)/2)*T)/(o*sqrt(T))
 # d2 = d1-o*sqrt(T)
+
+
+def calculate_d1_d2(S, K, T, r, sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return d1, d2
