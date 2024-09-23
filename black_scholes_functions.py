be46a3eb008e6ee46c67bd42d40ef610a445a399
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-23 13:12:59 +0100

    made black_scholes_put function

--- black_scholes_functions.py
+++ black_scholes_functions.py
@@ -31,3 +31,15 @@ def calculate_d1_d2(S, K, T, r, sigma):
     d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
     return d1, d2
+
+
+def black_scholes_call(S, K, T, r, sigma):
+    d1, d2 = calculate_d1_d2(S, K, T, r, sigma)
+    call_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    return call_price
+
+
+def black_scholes_put(S, K, T, r, sigma):
+    d1, d2 = calculate_d1_d2(S, K, T, r, sigma)
+    put_price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+    return put_price
