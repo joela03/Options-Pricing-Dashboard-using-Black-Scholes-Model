76df2e1c55ca9e527c86e5d96952c8799ad73b30
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-24 11:10:39 +0100

    added title and stock selector to dash application

--- app.py
+++ app.py
@@ -12,3 +12,25 @@ from load_financial_data import (read_sp500_table, fetch_current_stock_price,
                                  fetch_risk_free_rate)
 from black_scholes_functions import (calculate_d1_d2, black_scholes_call,
                                      black_scholes_put, delta, gamma, theta, vega, rho)
+
+# Fetch list of stocks
+sp500_list = read_sp500_table()
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1("Black-Scholes Option Pricing & Greeks Calculator"),
+            html.Label("Select Stock"),
+            dcc.Dropdown(
+                id='stock-dropdown',
+                options=[{'label': stock['name'], 'value': stock['ticker']}
+                         for stock in sp500_list],
+                value='AAPL')
+        ], width=4)
+    ])
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
