694cfc95bbea8113d89ebf4f54b21cfdb187e2c1
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-24 03:04:51 +0100

    made fetch_current_stock_price function

--- load_financial_data.py
+++ load_financial_data.py
@@ -1,6 +1,7 @@
 """This file contains functions that load data from API's"""
 
 import pandas as pd
+import yfinance as yf
 
 
 def read_sp500_table():
@@ -16,6 +17,19 @@ def read_sp500_table():
     return sp500_list
 
 
+def fetch_current_stock_price(ticker):
+    """Fetches most accurate stock price from yfinance library"""
+    stock = yf.Ticker(ticker)
+
+    recent_data = stock.history(period='1d')
+
+    if not recent_data.empty:
+        current_price = recent_data['Close'].iloc[-1]
+        return current_price
+    else:
+        raise ValueError(f"No data found for ticker {ticker}")
+
+
 if __name__ == "__main__":
-    table = read_sp500_table()
-    print(table)
+    price = fetch_current_stock_price('MM')
+    print(price)
