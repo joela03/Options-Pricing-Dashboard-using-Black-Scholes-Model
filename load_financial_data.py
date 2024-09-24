5a5a9bd9bf7ea4e9f447e8919d7813286e62ca92
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-24 02:51:46 +0100

    refactored read_sp500_table

--- load_financial_data.py
+++ load_financial_data.py
@@ -10,7 +10,12 @@ def read_sp500_table():
     table = pd.read_html(url)
     sp500_df = table[0]
 
-    sp500_dict = pd.Series(
-        sp500_df['Security'].values, index=sp500_df['Symbol']).todict()
+    sp500_list = sp500_df.apply(
+        lambda row: {'name': row['Security'], 'ticker': row['Symbol']}, axis=1).tolist()
 
-    return sp500_dict
+    return sp500_list
+
+
+if __name__ == "__main__":
+    table = read_sp500_table()
+    print(table)
