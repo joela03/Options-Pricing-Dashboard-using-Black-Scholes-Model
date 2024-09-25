0e1d13b8948fc6a5fac6c547f256947903968b09
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-25 13:08:34 +0100

    added borders to layout

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import dash
 from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
+import plotly.graph_objs as go
+import numpy as np
 import pandas as pd
 import yfinance as yf
 from dotenv import load_dotenv
@@ -21,42 +23,80 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
-            html.H1("Black-Scholes Option Pricing & Greeks Calculator"),
-            html.Label("Select Stock"),
-            dcc.Dropdown(
-                id='stock-dropdown',
-                options=[{'label': stock['name'], 'value': stock['ticker']}
-                         for stock in sp500_list],
-                value='AAPL'),
-            html.Label("Strike Price"),
-            dcc.Input(id='strike-price', type='number', value=100, step=0.01),
-            html.Label("Time to Maturity (Years)"),
-            dcc.Input(id='time-to-maturity',
-                      type='number', value=1, step=0.01),
-            html.Label("Volatility (σ)"),
-            dcc.Input(id='volatility', type='number', value=0.2, step=0.01),
-            html.Br(),
-            html.Button('Calculate', id='calculate-button', n_clicks=0)
+            html.H1("Option Pricing & Greeks Calculator",
+                    className="text-center"),
+            html.P("Select a stock and enter option parameters to calculate option prices and Greeks.",
+                   className="text-center")
+        ], width=12)
+    ]),
+    html.Hr(),
+    dbc.Row([
+        dbc.Col([
+            html.H5("Option Parameters"),
+            dbc.Card([
+                dbc.CardBody([
+                    html.Label("Select Stock"),
+                    dcc.Dropdown(
+                        id='stock-dropdown',
+                        options=[{'label': stock['name'], 'value': stock['ticker']}
+                                 for stock in sp500_list],
+                        value='AAPL',  # Default value
+                    ),
+                    html.Label("Strike Price"),
+                    dcc.Input(id='strike-price', type='number',
+                              value=100, step=0.01),
+                    html.Label("Time to Maturity (Years)"),
+                    dcc.Input(id='time-to-maturity',
+                              type='number', value=1, step=0.01),
+                    html.Label("Volatility (σ)"),
+                    dcc.Input(id='volatility', type='number',
+                              value=0.2, step=0.01),
+                    html.Label("Risk-Free Rate (r)"),
+                    dcc.Input(id='risk-free-rate', type='number',
+                              value=0.05, step=0.01),
+                    html.Br(),
+                    html.Br(),
+                    dbc.Button("Calculate", id='calculate-button',
+                               color="primary", n_clicks=0)
+                ])
+            ], className="mb-4")
         ], width=4),
         dbc.Col([
-            html.H3("Results"),
-            html.Div(id='results')
+            html.H5("Results"),
+            dbc.Card([
+                dbc.CardBody([
+                    html.Div(id='results')
+                ])
+            ])
         ], width=8)
+    ]),
+    html.Hr(),
+    dbc.Row([
+        dbc.Col([
+            html.H5("Option Call Price Over Time"),
+            dcc.Graph(id='call-price-over-time')
+        ], width=6),
+        dbc.Col([
+            html.H5("Option Put Price Over Time"),
+            dcc.Graph(id='put-price-over-time')
+        ], width=6)
     ])
-])
+], fluid=True)
 
 
 @app.callback(
-    Output('results', 'children'),
-    Input('calculate-button', 'n_clicks'),
-    Input('stock-dropdown', 'value'),
-    Input('strike-price', 'value'),
-    Input('time-to-maturity', 'value'),
-    Input('volatility', 'value')
+    [Output('results', 'children'),
+     Output('call-price-over-time', 'figure'),
+     Output('put-price-over-time', 'figure')],
+    [Input('calculate-button', 'n_clicks'),
+     Input('stock-dropdown', 'value'),
+     Input('strike-price', 'value'),
+     Input('time-to-maturity', 'value'),
+     Input('volatility', 'value')]
 )
 def calculate_options_and_greeks(n_clicks, ticker, K, T, sigma,):
     if n_clicks == 0:
-        return ""
+        return "", go.Figure()
 
     S = fetch_current_stock_price(ticker)
     api_key = os.environ['ALPHA_VANTAGE_API_KEY']
@@ -70,7 +110,7 @@ def calculate_options_and_greeks(n_clicks, ticker, K, T, sigma,):
     vega_value = vega(S, K, T, r, sigma)
     rho_call = rho(S, K, T, r, sigma, option_type='call')
 
-    return html.Div([
+    results = html.Div([
         html.P(f"Stock Price (S): {S:.2f}"),
         html.P(f"Call Option Price: {call_price:.2f}"),
         html.P(f"Put Option Price: {put_price:.2f}"),
@@ -80,6 +120,31 @@ def calculate_options_and_greeks(n_clicks, ticker, K, T, sigma,):
         html.P(f"Vega: {vega_value:.2f}"),
         html.P(f"Rho (Call): {rho_call:.2f}")
     ])
+
+    # Option Price of time graph
+    times = np.linspace(0.01, T, 100)
+    call_prices_over_time = [black_scholes_call(
+        S, K, t, r, sigma) for t in times]
+    put_prices_over_time = [black_scholes_put(
+        S, K, t, r, sigma) for t in times]
+
+    call_fig = go.Figure()
+    call_fig.add_trace(go.Scatter(x=times, y=call_prices_over_time,
+                                  mode='lines', name='Call Price'))
+    call_fig.update_layout(title="Call Option Prices Over Time to Maturity",
+                           xaxis_title="Time to Maturity (Years)",
+                           yaxis_title="Call Option Price",
+                           template="plotly_dark")
+
+    put_fig = go.Figure()
+    put_fig.add_trace(go.Scatter(x=times, y=put_prices_over_time,
+                                 mode='lines', name='Put Price'))
+    call_fig.update_layout(title="Put Option Prices Over Time to Maturity",
+                           xaxis_title="Time to Maturity (Years)",
+                           yaxis_title="Put Option Price",
+                           template="plotly_dark")
+
+    return results, call_fig, put_fig
 
 
 if __name__ == "__main__":
